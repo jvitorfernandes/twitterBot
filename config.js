af73082de4c0a7b54f5c05be0cd1ffdacf2ab250
João Vítor de Brito Fernandes <fernandesjvb@gmail.com>
2017-10-06 02:06:21 -0300

    using dotenv to store config variables safely

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 module.exports = {
-	consumer_key: 'j6z1FFRFunoX4NdMYTcE1ZIlY',
-	consumer_secret: 'f438Vs3Ch8y1MFmfNsCliWwauVnD4A6mI5YFKlkSiAb51oyx1r',
-	access_token: '916142311414870016-7MP5luQYssgvvA5hwJt9Jlhn0rPsgUN',
-	access_token_secret: 'J5n0JGS2g6d66XVauoOlHdAj2MFyRMgBpvDELzFQSGH6W'
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET, 
+	access_token: process.env.ACCESS_TOKEN, 
+	access_token_secret: process.env.ACCESS_TOKEN_SECRET
 }
