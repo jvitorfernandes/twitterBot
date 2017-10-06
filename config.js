9571cb8f0e53e10f058ced4a45db68a8cc548ad6
João Vítor de Brito Fernandes <fernandesjvb@gmail.com>
2017-10-06 00:56:43 -0300

    config to heroku

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: 'j6z1FFRFunoX4NdMYTcE1ZIlY',
+	consumer_secret: 'f438Vs3Ch8y1MFmfNsCliWwauVnD4A6mI5YFKlkSiAb51oyx1r',
+	access_token: '916142311414870016-7MP5luQYssgvvA5hwJt9Jlhn0rPsgUN',
+	access_token_secret: 'J5n0JGS2g6d66XVauoOlHdAj2MFyRMgBpvDELzFQSGH6W'
+}
