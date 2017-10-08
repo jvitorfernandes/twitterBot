a3742ecbdc9e9ee0652657378fb3175baf5d49e2
João Vítor de Brito Fernandes <fernandesjvb@gmail.com>
2017-10-08 02:07:57 -0300

    send a tweet when getting mentioned

--- bot.js
+++ bot.js
@@ -5,17 +5,29 @@ var bot = new Bot();
 var r = Math.floor(Math.random()*100);
 
 bot.stream.on('follow', followed); //calls method anytime someone follows me
+bot.stream.on('tweet', tweetEvent);
 
-	//do something when someone follows
-	function followed(eventMsg){
+//do something when someone follows
+function followed(eventMsg){
 
-		var name = eventMsg.source.name;
-		var screenName = eventMsg.source.screen_name;
-		console.log(name + " followed")
-		var r = Math.floor(Math.random()*100);
-		bot.tweetIt("This is a test. " + r);
-		bot.sendDM(eventMsg.source.id, "Hi, " + name + "! Thanks for following!");
+	var name = eventMsg.source.name;
+	var screenName = eventMsg.source.screen_name;
+	console.log(name + " followed");
+	var r = Math.floor(Math.random()*100);
+	bot.tweetIt("This is a test. " + r);
+	bot.sendDM(eventMsg.source.id, "Hi, " + name + "! Thanks for following!");
 
-	};
+};
 
+//replys a tweet
+function tweetEvent(eventMsg){
+	var replyto = eventMsg.in_reply_to_screen_name;
+	var text = eventMsg.text;
+	var from = eventMsg.user.screen_name;
 
+	console.log(replyto, from);
+
+	if(replyto === 'bot_senario'){
+		bot.tweetIt("@" + from + " thanks for tweeting me!");
+	}
+};
